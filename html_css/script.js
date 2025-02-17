fbc49f0d37db59520526b69553d2a3c31f678513
amani <amounaeid1@gmail.com>
2025-02-16 20:55:10 -0800

    initial commit

---
+++ html_css/script.js
@@ -0,0 +1,55 @@
+/*studentName="Ahmad";
+console.log(studentName);
+age= 44;
+console.log(age);
+var age= 88
+console.log(age);
+var age=10;
+console.log(age);
+let a=10;
+console.log(a);
+function myfunction()
+{
+    let a=55;
+    console.log(a);
+}
+const students=["elie","alex","alice"];
+    students[0]="";
+    console.log(students);
+console.log(13%4);
+let i=4;
+console.log(i);
+i++;
+console.log(i);
+i+=1;
+console.log(i);
+i=i+1;
+console.log(i);
+let j=6;
+console.log(j);
+j--;
+console.log(j);
+j-=1;
+console.log(j);
+j=j-1;
+console.log(j);
+console.log(3==="3");
+function helloworld()
+{
+    console.log("welcome to the first course");
+}
+helloworld();
+ function addnumbers(a,b)
+ {
+    return a+b;
+ }
+ console.log(addnumbers(10,6));*
+ let addnumbers = (a, b ) =>
+ {
+    return  a + b;
+ };
+console.log(addnumbers(10,60));
+let addnumb = (a,b) => a+b;
+console.log(addnumb(10,60));*/
+let name= "eve";
+let helloworld= name => console.log("hello", name);
